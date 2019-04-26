c221c0fe8d8be6363ae84f45b826b667a02cc22f
mac <smithovich@gmail.com>
2019-04-26 16:20:18 +0300

    new start

--- objects_and_classes/homework/homework.py
+++ objects_and_classes/homework/homework.py
@@ -50,56 +50,48 @@ from typing import Union, List
 from constants import CARS_TYPES, CARS_PRODUCER
 import uuid
 import random
+CT = Union[float, str, str, type(uuid.uuid4), float]
 
 
 class Cesar:
+    pass
 
-    def __init__(self, price, type_car, producer):
-        self.__price = price
-        self.__type = type_car
-        self.__producer = producer
+
+class Car:
+
+    def __init__(self, price: float, type, producer):
+        self.price = price
+        self.type = type
+        self.producer = producer
         self.number = uuid.uuid4()
-        self.mileage = float(f'{random.random() * 100:2f}')
-
-    @property
-    def price(self):
-        assert isinstance(self.__price, float), 'Price must be float'
-        return self.__price
-
-    @property
-    def type(self):
-        car = self.__type if self.__type in CARS_TYPES else None
-        assert car, f'Select car in list {CARS_TYPES}'
-        return car
-
-    @property
-    def producer(self):
-        producer = self.__producer if self.__producer in CARS_PRODUCER else None
-        assert producer, f'Select producer in list {CARS_PRODUCER}'
-        return producer
+        self.mileage = f'{random.random() * 100:2f}'
 
     @property
     def new_number(self):
         self.number = uuid.uuid4()
         return self.number
 
-    @property
-    def logs(self):
-        specification = dict(
-            price=self.price,
-            type=self.type,
-            producer=self.producer,
-            number=str(self.number),
-            mileage=self.mileage
-        )
-        return specification
-
     def __str__(self):
-        return self.car()
+        return f'Specification for the car next: \n Price {self.price} \n Type {self.type} \
+        \n Producer {self.producer} \n Number {self.number} \n Mileage {self.mileage}'
 
+    def __float__(self):
+        return float(self.mileage), float(self.price)
 
-class Car:
-    pass
+    def __lt__(self, other):
+        return other.price < self.price
+
+    def __gt__(self, other):
+        return other.price > self.price
+
+    def __eq__(self, other):
+        return other.price == self.price
+
+    def __le__(self, other):
+        return other.price <= self.price
+
+    def __ge__(self, other):
+        return other.price >= self.price
 
 
 class Garage:
@@ -108,13 +100,17 @@ class Garage:
 
 if __name__ == '__main__':
 
-    bmw = Cesar(price=35000.00, type_car='Truck', producer='BMW')
-    ford = Cesar(price=25000.145, type_car='Sedan', producer='FORD')
-    print(bmw.price, bmw.type, bmw.producer, bmw.number)
-    print(f'bmw number is {bmw.number}')
-    print(f'bmw number has been changed to the {bmw.new_number}')
-    print('set of the car')
-    print('___________')
-    for k, v in bmw.logs.items(): print(f'{k} {v}')
-    print('___________')
-    print(bmw.price > ford.price)
+    bmw = Car(price=35000.00, type='Truck', producer='BMW')
+    # ford = Car(price=25000.145, type_car='Sedan', producer='FORD')
+    # print(bmw.price, bmw.type, bmw.producer, bmw.number)
+    print(bmw)
+    # print(f'bmw number is {bmw.number}')
+    # print(f'bmw number has been changed to the {bmw.new_number}')
+    # print(bmw)
+    # print(type(uuid))
+    # print('___________')
+    # for k, v in bmw.logs.items(): print(f'{k} {v}')
+    # print('___________')
+    # print(bmw.price + ford.price)
+    # # print(bmw.mileage + ford.mileage)
+    # print(type(bmw.mileage))
