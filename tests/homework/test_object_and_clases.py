6a9cf50cf7256314c007e032ef112369c7a63ed5
mac <smithovich@gmail.com>
2019-06-21 00:24:59 +0300

    fix attributes

--- tests/homework/test_object_and_clases.py
+++ tests/homework/test_object_and_clases.py
@@ -30,11 +30,13 @@ class Base(unittest.TestCase):
             producer=random.choice(CARS_PRODUCER),
             mileage=random.randint(10, 1000) * 1.2
         )
+        self.first_car, self.second_car, self.third_car = YAML_CARS
         self.cesar_yaml = Cesar('Pit', YAML_GARAGES)
         self.cesar_json = JSON_CESAR
         self.cesar_pickle = PICKLE_CESAR
         self.garage = Garage(town='Amsterdam', places=3)
         self.cesar = Cesar('Denchick', [self.garage])
+        self.first_garage, self.second_garage, self.third_garage = YAML_GARAGES
 
 
 class TestCar(Base):
@@ -85,12 +87,11 @@ class TestGarage(Base):
 
     def test_remove_car(self):
         self.add_car()
-        get_car = YAML_CARS[0]
         self.assertEqual(len(self.garage.cars), self.garage.places)
-        self.garage.remove(get_car)
+        self.garage.remove(self.first_car)
         self.assertEqual(len(self.garage.cars), 2)
         for car in self.garage.cars:
-            self.assertIsNot(car.number, get_car.number)
+            self.assertIsNot(car.number, self.first_car.number)
 
     def test_hit_hat(self):
         self.add_car()
@@ -105,19 +106,20 @@ class TestCesar(Base):
         return self.garage
 
     def test_add_car(self):
-        self.assertEqual(self.cesar_yaml.garages[2].cars, [])
+        self.assertEqual(self.third_garage.cars, [])
         self.cesar_yaml.add_car(self.car)
-        self.assertEqual(len(self.cesar_yaml.garages[2].cars), 1)
-        self.assertEqual(
-            self.car.number, self.cesar_yaml.garages[2].cars[0].number)
+        self.assertEqual(len(self.third_garage.cars), 1)
+        self.assertEqual(self.car.number, self.third_garage.cars[0].number)
 
     def test_hit_hat(self):
         self.add_car()
         self.assertEqual(self.cesar.hit_hat(), 216482.94499999998)
 
     def test_yaml_hit_hat(self):
-        self.cesar_yaml.add_car(self.car)
-        self.assertEqual(self.cesar_yaml.hit_hat(), 136804.8)
+        self.cesar_yaml.add_car(self.first_car)
+        self.car.price = 225609.6
+        self.cesar.add_car(self.car)
+        self.assertEqual(self.cesar_yaml, self.cesar)
 
     def test_garages_count(self):
         assert len(self.cesar_yaml.garages) == self.cesar_yaml.garages_count()
@@ -135,22 +137,24 @@ class TestCesar(Base):
         assert sum(cars_count) == self.cesar_pickle.cars_count()
 
     def test_validation_add_cars(self):
-        self.cesar_yaml.add_car(self.car, garage=self.cesar_yaml.garages[1])
+        self.cesar_yaml.add_car(self.car, garage=self.second_garage)
         self.cesar_yaml.add_car(
-            YAML_CARS[0], garage=self.cesar_yaml.garages[1]
+            self.first_car, garage=self.second_garage
         )
         message = 'Sorry garage count is full (places=2, count=2)'
         assert self.cesar_yaml.add_car(
-            YAML_CARS[1], garage=self.cesar_yaml.garages[1]) == message
+           self.second_car, garage=self.second_garage) == message
 
     def test_gt_cesar_from_yaml(self):
         new_cesar = Cesar('Anton', YAML_GARAGE)
         new_cesar.add_car(self.car)
-        new_cesar.add_car(YAML_CARS[0])
+        new_cesar.add_car(self.first_car)
         assert new_cesar > self.cesar_yaml
 
     def test_json_eq_cesar(self):
-        print(self.cesar_json.hit_hat())
+        self.car.price = 134673.235
+        self.cesar.add_car(self.car)
+        self.assertEqual(self.cesar, self.cesar_json)
 
 
 if __name__ == "__main__":
